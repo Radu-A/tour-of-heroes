d6b93991b85d5924fecfcf27be7051cbd10d6fee
Radu-A <elcastravete@gmail.com>
2026-01-16 09:39:47 +0100

    chore: clean preloaded html template

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('tour-of-heroes');
+  protected readonly title = signal('Tour of Heroes');
 }
